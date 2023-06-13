14b4fc81cbb51705e7885f301aff0d2d817c0647
rsundar92 <64267752+rsundar92@users.noreply.github.com>
2023-06-13 21:37:06 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import "destyle.css";
+
+import "./App.css";
+import CatMarket from "./CatMarket";
+
+function App() {
+  return (
+    <div className="App">
+      <CatMarket />
+    </div>
+  );
+}
+
+export default App;
